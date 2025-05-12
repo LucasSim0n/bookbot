80873149d9125b298a084462cb84efbb666f0f1e
LucasSim0n <lucassimonfeijoo@gmail.com>
2025-05-12 18:38:23 +0200

    char count report

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import stats
+from stats import get_book_text, get_char_count, get_num_words, get_report, sort_on
 
 
 def main():
-    text = stats.get_book_text("books/frankenstein.txt")
-    num_words = stats.get_book_words(text)
-    print(f"{num_words} words found in the document")
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    num_words = get_num_words(text)
+    char_repetition = get_char_count(text)
+    report = get_report(char_repetition)
+    report.sort(reverse=True, key=sort_on)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in report:
+
+        print(f"{char["char"]}: {char["num"]}")
+
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -3,5 +3,28 @@ def get_book_text(path):
         return f.read()
 
 
-def get_book_words(text):
+def get_num_words(text):
     return len(text.split())
+
+
+def get_char_count(text):
+    chars = {}
+    for char in text.lower():
+        chars[char] = chars.get(char, 0) + 1
+
+    return chars
+
+
+def get_report(chars):
+    report = []
+    for char in chars:
+        if not char.isalpha():
+            continue
+        rep_char = {"char": char, "num": chars[char]}
+        report.append(rep_char)
+
+    return report
+
+
+def sort_on(report):
+    return report["num"]
