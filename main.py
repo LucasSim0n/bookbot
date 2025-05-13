fd8f8216d1056492bb5ec8e9b43f79f61f1a2b1d
LucasSim0n <lucassimonfeijoo@gmail.com>
2025-05-13 12:45:02 +0200

    Arg parser

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+
 from stats import get_book_text, get_char_count, get_num_words, get_report, sort_on
 
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("[+]Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_num_words(text)
     char_repetition = get_char_count(text)
